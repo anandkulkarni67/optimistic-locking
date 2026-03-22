a9557e37600bd9ed1b9d08ef51028b25f1b75ca7
Anand Kulkarni <anandkulkarni67@gmail.com>
2026-03-21 17:27:56 -0700

    update error handling

--- api/src/routes/v1/Customer.route.ts
+++ api/src/routes/v1/Customer.route.ts
@@ -4,43 +4,23 @@ import { customerService } from '../../service/Customer.service';
 const router = Router();
 
 router.get('/:id', async (req: Request, res: Response) => {
-  try {
     const customerMetadata = await customerService.getCustomer(req.params.id.toString());
     res.status(200).json(customerMetadata);
-  } catch (error) {
-    console.error('An error ocurred:', error);
-    res.status(500).json(error);
-  }
 });
 
 router.post('/', async (req: Request, res: Response) => {
-  try {
     const customerMetadata = await customerService.addCustomer(req.body);
     res.status(200).json(customerMetadata);
-  } catch (error) {
-    console.error('An error ocurred:', error);
-    res.status(500).json(error);
-  }
 });
 
 router.put('/:id', async (req: Request, res: Response) => {
-  try {
     const customerMetadata = await customerService.updateCustomer(req.params.id.toString(), req.body);
     res.status(200).json(customerMetadata);
-  } catch (error) {
-    console.error('An error ocurred:', error);
-    res.status(500).json(error);
-  }
 });
 
 router.delete('/:id', async (req: Request, res: Response) => {
-  try {
     await customerService.deleteCustomer(req.params.id.toString(), Number(req.query.version));
     res.status(200).send();
-  } catch (error) {
-    console.error('An error ocurred:', error);
-    res.status(500).json(error);
-  }
 });
 
 export default router;
--- api/src/routes/v1/Healthcheck.route.ts
+++ api/src/routes/v1/Healthcheck.route.ts
@@ -6,12 +6,7 @@ import { Service } from '../../model/data/Service';
 const router = Router();
 
 router.get('/app', async (req: Request, res: Response) => {
-  try {
     res.status(200).json(new Status(Service.APPLICATION, ServiceStatusValue.UP));
-  } catch (error) {
-    console.error('An error ocurred:', error);
-    res.status(500).json(error);
-  }
 });
 
 export default router;
